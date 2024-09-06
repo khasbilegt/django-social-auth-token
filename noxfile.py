2feaca0b32c2f280b3776c2aaa7ae455544d383f
khasbilegt <khasbilegt.ts@gmail.com>
2024-09-07 03:13:29 +0800

    update dependencies

--- noxfile.py
+++ noxfile.py
@@ -2,19 +2,12 @@ import nox
 
 
 @nox.session
-def lint(session):
-    session.run("black", "--check", ".")
-    session.run("flake8", ".")
-    session.run("isort", "-q", ".")
-
-
-@nox.session
-@nox.parametrize("django", ["3.2", "4.0", "main"])
+@nox.parametrize("django", ["4.2", "5.0", "main"])
 def test(session, django):
     if django == "main":
-        session.install("https://github.com/django/django/archive/main.tar.gz")  # noqa E501
+        session.run("pip", "install", "https://github.com/django/django/archive/main.tar.gz")  # noqa E501
     else:
-        session.install(f"django=={django}")
+        session.run("pip", "install", f"django=={django}")
     session.run("coverage", "run", "-m", "pytest", "tests")
     session.run("coverage", "report")
     session.run("coverage", "xml")
