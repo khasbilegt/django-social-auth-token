9dc6fbdb6307fc1c3fb1de240d51d8220229e54b
khasbilegt <khasbilegt.ts@gmail.com>
2024-09-07 03:32:58 +0800

    correct version matrix

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,18 @@
+import sys
+
 import nox
 
 
 @nox.session
 @nox.parametrize("django", ["4.2", "5.0", "main"])
 def test(session, django):
-    if django == "main":
-        session.run("pip", "install", "https://github.com/django/django/archive/main.tar.gz")  # noqa E501
+    if django != "4.2" and not sys.version_info.minor >= 10:
+        session.skip()
     else:
-        session.run("pip", "install", f"django=={django}")
-    session.run("coverage", "run", "-m", "pytest", "tests")
-    session.run("coverage", "report")
-    session.run("coverage", "xml")
+        if django == "main":
+            session.run("pip", "install", "https://github.com/django/django/archive/main.tar.gz")  # noqa E501
+        else:
+            session.run("pip", "install", f"django=={django}")
+        session.run("coverage", "run", "-m", "pytest", "tests")
+        session.run("coverage", "report")
+        session.run("coverage", "xml")
